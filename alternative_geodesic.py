bb720f1b401d51e604b023657d12d07dcf058b91
Ayan <ayanbanerjee7777@gmail.com>
2020-03-08 19:14:15 +0530

    Lint fixes

--- alternative_geodesic.py
+++ alternative_geodesic.py
@@ -1,25 +1,28 @@
 # -*- coding: utf-8 -*-
 #
 #
-# TheVirtualBrain-Framework Package. This package holds all Data Management, and
-# Web-UI helpful to run brain-simulations. To use it, you also need do download
-# TheVirtualBrain-Scientific Package (for simulators). See content of the
-# documentation-folder for more details. See also http://www.thevirtualbrain.org
+# TheVirtualBrain-Framework Package. This package holds all Data Management,
+# and Web-UI helpful to run brain-simulations. To use it, you also need do
+# download TheVirtualBrain-Scientific Package (for simulators). See content of
+# the documentation-folder for more details. See also
+# http://www.thevirtualbrain.org
 #
 # (c) 2012-2017, Baycrest Centre for Geriatric Care ("Baycrest") and others
 #
-# This program is free software: you can redistribute it and/or modify it under the
-# terms of the GNU General Public License as published by the Free Software Foundation,
-# either version 3 of the License, or (at your option) any later version.
-# This program is distributed in the hope that it will be useful, but WITHOUT ANY
-# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
-# PARTICULAR PURPOSE.  See the GNU General Public License for more details.
-# You should have received a copy of the GNU General Public License along with this
-# program.  If not, see <http://www.gnu.org/licenses/>.
+# This program is free software: you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free
+# Software Foundation, either version 3 of the License, or (at your option)
+# any later version. This program is distributed in the hope that it will be
+# useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General
+# Public License for more details. You should have received a copy of the GNU
+# General Public License along with this program. If not, see
+# <http://www.gnu.org/licenses/>.
 #
 #
 #   CITATION:
-# When using The Virtual Brain for scientific publications, please cite it as follows:
+# When using The Virtual Brain for scientific publications, please cite it as
+# follows:
 #
 #   Paula Sanz Leon, Stuart A. Knock, M. Marmaduke Woodman, Lia Domide,
 #   Jochen Mersmann, Anthony R. McIntosh, Viktor Jirsa (2013)
@@ -160,10 +163,10 @@ class HalfEdge(object):
             return x.vertex_0 < y.vertex_0
 
     def __ne__(x, y):
-        return x.vertex_0 != y.vertex_0 or x.vertex_1 != y.vertex_1;
+        return x.vertex_0 != y.vertex_0 or x.vertex_1 != y.vertex_1
 
     def __eq__(x, y):
-        return x.vertex_0 == y.vertex_0 and x.vertex_1 == y.vertex_1;
+        return x.vertex_0 == y.vertex_0 and x.vertex_1 == y.vertex_1
 
 
 class SurfacePath(object):
@@ -178,11 +181,6 @@ class SurfacePath(object):
 
 
 # geodesic_algorithm_base.h
-
-
-
-
-
 
 
 class Base(object):
@@ -227,6 +225,3 @@ class Subdivision(Base):
 
 class Exact(Base):
     pass
-
-
-
