dcf6c81fe2aafec7154d036a0c77cc55bc2572f8
ayan-b <ayanbanerjee7777@gmail.com>
2020-05-18 11:42:35 +0530

    Use c++14 setup.py

--- setup.py
+++ setup.py
@@ -54,7 +54,9 @@ GEODESIC_MODULE = [
         sources=["gdist.pyx"],  # Filename of Cython source
         language="c++",  # Cython create C++ source
         # Disable assertions; one is failing geodesic_mesh.h:405
-        define_macros=[('NDEBUG', 1)]
+        define_macros=[('NDEBUG', 1)],
+        extra_compile_args=['--std=c++14'],
+        extra_link_args=['--std=c++14'],
     )
 ]
 
