862dc80b70f9003f96e2b12bfa1eea4e80069a10
ayan-b <ayanbanerjee7777@gmail.com>
2020-05-19 22:30:04 +0530

    Add another test

--- tests/test_gdist.py
+++ tests/test_gdist.py
@@ -12,3 +12,14 @@ def test_flat_triangular_mesh():
     distance = gdist.compute_gdist(vertices, triangles,
                                    source_indices=source, target_indices=target)
     np.testing.assert_array_almost_equal(distance, [0.2])
+
+
+def test_hedgehog_mesh():
+    data = np.loadtxt("data/hedgehog_mesh.txt", skiprows=1)
+    vertices = data[0:300].astype(np.float64)
+    triangles = data[300:].astype(np.int32)
+    source = np.array([0], dtype=np.int32)
+    target = np.array([1], dtype=np.int32)
+    distance = gdist.compute_gdist(vertices, triangles,
+                                   source_indices=source, target_indices=target)
+    np.testing.assert_array_almost_equal(distance, [1.40522])
