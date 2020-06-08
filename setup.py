12f7779627b762e31b282eaa5dc9496f7d46af41
liadomide <lia_domide@yahoo.com>
2020-06-08 23:32:41 +0300

    TVB-2730 First draft to biold tvb-gdist whl also on Mac

--- setup.py
+++ setup.py
@@ -74,7 +74,7 @@ with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as fd:
 
 setuptools.setup(
     name="tvb-" + GEODESIC_NAME,
-    version='2.0.5',
+    version='2.0.2',
     ext_modules=GEODESIC_MODULE,
     include_dirs=INCLUDE_DIRS,
     cmdclass={'build_ext': build_ext},
