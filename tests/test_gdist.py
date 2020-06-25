e6f6e19a5132391596d4f6e660ae48dd81347bc5
ayan-b <ayanbanerjee7777@gmail.com>
2020-06-25 08:44:18 +0530

    Add a test for 1-indexed case

--- tests/test_gdist.py
+++ tests/test_gdist.py
@@ -18,6 +18,24 @@ class TestComputeGdist():
         )
         np.testing.assert_array_almost_equal(distance, [0.2])
 
+    def test_flat_triangular_mesh_1_indexed(self):
+        data = np.loadtxt(
+            "data/flat_triangular_mesh_1_indexed.txt",
+            skiprows=1,
+        )
+        vertices = data[0:121].astype(np.float64)
+        triangles = data[121:].astype(np.int32)
+        source = np.array([2], dtype=np.int32)
+        target = np.array([3], dtype=np.int32)
+        distance = gdist.compute_gdist(
+            vertices,
+            triangles,
+            source,
+            target,
+            is_one_indexed=True,
+        )
+        np.testing.assert_array_almost_equal(distance, [0])
+
     def test_hedgehog_mesh(self):
         data = np.loadtxt("data/hedgehog_mesh.txt", skiprows=1)
         vertices = data[0:300].astype(np.float64)
@@ -39,6 +57,28 @@ class TestLocalGdistMatrix:
         vertices = data[0:121].astype(np.float64)
         triangles = data[121:].astype(np.int32)
         distances = gdist.local_gdist_matrix(vertices, triangles)
+        epsilon = 1e-6  # the default value used in `assert_array_almost_equal`
+        # test if the obtained matrix is symmetric
+        assert (abs(distances - distances.T) > epsilon).nnz == 0
+        np.testing.assert_array_almost_equal(distances.toarray()[1][0], 0.2)
+        # set max distance as 0.3
+        distances = gdist.local_gdist_matrix(vertices, triangles, 0.3)
+        # test if the obtained matrix is symmetric
+        assert (abs(distances - distances.T) > epsilon).nnz == 0
+        assert np.max(distances) <= 0.3
+
+    def test_flat_triangular_mesh_1_indexed(self):
+        data = np.loadtxt(
+            "data/flat_triangular_mesh_1_indexed.txt",
+            skiprows=1,
+        )
+        vertices = data[0:121].astype(np.float64)
+        triangles = data[121:].astype(np.int32)
+        distances = gdist.local_gdist_matrix(
+            vertices,
+            triangles,
+            is_one_indexed=True
+        )
         epsilon = 1e-6  # the default value used in `assert_array_almost_equal`
         # test if the obtained matrix is symmetric
         assert (abs(distances - distances.T) > epsilon).nnz == 0
