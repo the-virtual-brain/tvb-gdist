72fefcc2a12088ea782fee91f0b5850a962385a6
Ayan <ayanbanerjee7777@gmail.com>
2020-07-11 19:34:40 +0530

    Add more surface data tests

--- tests/test_equality_with_stable.py
+++ tests/test_equality_with_stable.py
@@ -4,19 +4,20 @@ import gdist
 
 
 def test_equality_with_stable():
-    surface_data = 'inner_skull_642'
-    expected = np.loadtxt(f'data/{surface_data}/gdist_matrix.txt')
-    vertices = np.loadtxt(
-        f'data/{surface_data}/vertices.txt',
-        dtype=np.float64,
-    )
-    triangles = np.loadtxt(
-        f'data/{surface_data}/triangles.txt',
-        dtype=np.int32,
-    )
-    actual = gdist.local_gdist_matrix(
-        vertices=vertices,
-        triangles=triangles,
-    )
-    actual = actual.toarray()
-    np.testing.assert_array_almost_equal(actual, expected)
+    surface_datas = ['inner_skull_642', 'outer_skull_642', 'scalp_1082']
+    for surface_data in surface_datas:
+        expected = np.loadtxt(f'data/{surface_data}/gdist_matrix.txt')
+        vertices = np.loadtxt(
+            f'data/{surface_data}/vertices.txt',
+            dtype=np.float64,
+        )
+        triangles = np.loadtxt(
+            f'data/{surface_data}/triangles.txt',
+            dtype=np.int32,
+        )
+        actual = gdist.local_gdist_matrix(
+            vertices=vertices,
+            triangles=triangles,
+        )
+        actual = actual.toarray()
+        np.testing.assert_array_almost_equal(actual, expected)
