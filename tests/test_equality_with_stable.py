e677b682398ec54d57c1989b842becb25e9dcd49
ayan-b <ayanbanerjee7777@gmail.com>
2020-07-14 22:22:09 +0530

    Fix merge conflict

--- tests/test_equality_with_stable.py
+++ tests/test_equality_with_stable.py
@@ -4,26 +4,6 @@ import gdist
 
 
 def test_equality_with_stable():
-<<<<<<< HEAD
-    surface_datas = ['inner_skull_642', 'outer_skull_642', 'scalp_1082']
-    for surface_data in surface_datas:
-        expected = np.loadtxt(
-            f'data/surface_data/{surface_data}/gdist_matrix.txt')
-        vertices = np.loadtxt(
-            f'data/surface_data/{surface_data}/vertices.txt',
-            dtype=np.float64,
-        )
-        triangles = np.loadtxt(
-            f'data/surface_data/{surface_data}/triangles.txt',
-            dtype=np.int32,
-        )
-        actual = gdist.local_gdist_matrix(
-            vertices=vertices,
-            triangles=triangles,
-        )
-        actual = actual.toarray()
-        np.testing.assert_array_almost_equal(actual, expected)
-=======
     surface_data = 'inner_skull_642'
     expected = np.loadtxt(f'data/{surface_data}/gdist_matrix.txt')
     vertices = np.loadtxt(
@@ -40,4 +20,3 @@ def test_equality_with_stable():
     )
     actual = actual.toarray()
     np.testing.assert_array_almost_equal(actual, expected)
->>>>>>> ed49909... Use pytest again
