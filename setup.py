37b6ff6b0f000f2b38a986be0460f8fb36df5ff2
Marmaduke Woodman <marmaduke.woodman@univ-amu.fr>
2022-12-08 20:18:35 +0100

    temporarily switch pkg name to gdist to test pypi push

--- setup.py
+++ setup.py
@@ -104,7 +104,8 @@ class new_build_ext(build_ext):
 
 
 setuptools.setup(
-    name="tvb-" + GEODESIC_NAME,
+    #name="tvb-" + GEODESIC_NAME,
+    name=GEODESIC_NAME,
     version="2.1.0",
     ext_modules=GEODESIC_MODULE,
     include_dirs=INCLUDE_DIRS,
